75ae19a8bf109dc0b69cfa9830967187d5a4947c
Stephanie <stephanie@Stephanies-Mac.local>
2014-11-24 14:33:34 -0600

    Edited w the argv/c commands

--- highlow.php
+++ highlow.php
@@ -1,13 +1,26 @@
 <?php
+// Check for three arguments
+if ($argc == 3) {
+    echo "arg1 is {$argv[1]} and arg2 is {$argv[2]}\n";
 
-$ran = mt_rand(0, 100);
-fwrite (STDOUT, 'Please guess a number:');
+    // Assign values from arguments
+	$min = ($argv[1]);
+    $max = ($argv[2]);
+
+    // Generate a random number from min/max
+    $ran = mt_rand($min, $max);
+}
+
+else {
+	$ran = mt_rand(0, 100);
+}
+
 
 do {
-	
+	fwrite(STDOUT, 'Please guess a number:');
 	$guess = trim(fgets(STDIN));
 
-	if (!(is_numeric($guess))) 
+	if (is_numeric($guess))
 	{
 		if ($guess > $ran) {
 			echo "LOWER \n";
@@ -15,11 +28,12 @@ do {
 		elseif ($guess < $ran) {
 			echo "HIGHER \n";
 		}
-		else {
-			echo "Use a number please! \n";
-		}
 	}
+else {
+	echo "Use a number please! \n";
+	}
+
 } while ($guess != $ran); 
 	echo "GOOD GUESS \n";
 
-
+?>
